0812c9574dbb7a0655de92eb15433c3aa44ebf2e
James Beasley <jbeasley@alumni.cmu.edu>
2018-02-26 00:00:43 -0600

    n/a

--- sdcp11/src/main.cpp
+++ sdcp11/src/main.cpp
@@ -10,15 +10,16 @@
 //#include "Eigen-3.3/Eigen/QR"
 
 //includes
+#include <stdlib.h>              //using for "EXIT_..." macros
 #include "simulatorserver.h"
 #include "pathplanner.h"
 
 //function declarations
-void main(const int, const char**);
+int main(const int, const char**);
 
 //function definition
 //main thread of execution
-void main(const int argc, const char** argv)
+int main(const int argc, const char** argv)
 {
     //local vars
     SimulatorServer simulator_server;   //communication agent between the Udacity simulator and the path planner module
@@ -30,4 +31,7 @@ void main(const int argc, const char** argv)
     //start the simulator server, to listen for Udacity simulator connections,
     //once connected forward telemetry to the path planner and have it return back trajectories to the simulator
     simulator_server.start(path_planner);
+
+    //exit program, clean return code
+    return EXIT_SUCCESS;
 }
